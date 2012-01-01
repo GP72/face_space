ca631de39504020f82fd131bfb720eda776976d1
Graeme Phelan <greyphelan@gmail.com>
2012-01-01 15:26:53 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FaceSpace::Application.config.secret_token = '55cddf0fddeff90b5c53a14cdf1ee1a69912a7fc228da0dd66c87af2b14f81cd7c4ca45a455b6e7f219aa57ea7091a2afe25db50764f579cbf54cc838e56cd91'
